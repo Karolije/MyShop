bd2cf1171aa6bafd47281929fc470987fdbc0872
Karolina <karolina.jedrajczyk@gmail.com>
2025-08-04 10:22:12 +0200

    start app

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type User = {
+    id: number;
+    firstName: string;
+    lastName: string;
+    birthDate: string;
+    login: string;
+    email: string;
+    passwordHash: string; 
+  };
+  
